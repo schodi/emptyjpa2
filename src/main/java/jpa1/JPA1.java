dc624aae5127db22e7179a8b91d76a527551d913
Clemens Eisserer <ceisserer@gmail.com>
2025-10-22 17:11:50 +0200

    first commit

---
+++ src/main/java/jpa1/JPA1.java
@@ -0,0 +1,16 @@
+package jpa1;
+
+import java.util.List;
+import jakarta.persistence.*;
+
+import jakarta.persistence.Query;
+import jpa1.entities.*;
+
+
+public class JPA1 {
+    public JPA1() {
+    }
+    
+    public static void main(String[] args) throws Exception {
+    }
+}
